bed59104e8f7a01648ab29797783bbdd2fab34f2
Mahmoud-1764 <mahmoudchuaibsuliman@gmail.com>
2026-06-17 14:08:49 +0200

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Header() {
+  return (
+    
+      <header>
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">Projects</a></li>
+          <li>MAHMOUD</li>
+          <li><a href="#projects">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </header>
+      
+   
+  );
+}
+
+export default Header; 

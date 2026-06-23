b494672d7e06fd35a4a2965df0d13d26852b475c
Mahmoud-1764 <mahmoudchuaibsuliman@gmail.com>
2026-06-23 18:48:01 +0200

    font

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,14 +2,23 @@ import React from 'react';
 import './Home.css';
 
 function Home() {
-return (
-    <section className="home">
-        <h1 className="home-title">Web</h1>
-        <h1 className="home-title">Portfolio</h1>
-        <h1 className="home-title">Developer</h1>
-    </section>
+    return (
+        <>
+            <section className="home">
+                <h1 className="home-title">Web</h1>
+                <h1 id="portfolio">Portfolio</h1>
+                <h1 className="home-title">Developer</h1>
+            </section>
 
-)
+            <section className="scroll-indicator">
+                <div className="scroll-indicator__line"></div>
+                {/* <div id="scroll-indicator__fill" className="scroll-indicator__fill"></div> */}
+                <div className="scroll-indicator__line"></div>
+                <div className="scroll-indicator__line"></div>
+                <div className="scroll-indicator__line"></div>
+            </section>
+        </>
+    );
 }
 
 export default Home; 
